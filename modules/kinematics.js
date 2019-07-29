1047f10f3dcd09b9f2cfae9a47d77999b4263450
Oula Kivalo <oula.kivalo@aalto.fi>
2019-07-29 18:20:44 +0300

    Clean kinematics.js

--- modules/kinematics.js
+++ modules/kinematics.js
@@ -1,13 +1,14 @@
-//export const name = "kinematics";
+// Generates a snake like structure that follows the mouse
 import { Vec2 } from "./mathutils.js";
 
 
 let handle;
-const segments    = 75;
-const segmentSize = 20;
+const segments    = 200;
+const segmentSize = 10;
 const snake       = new Array(segments);
 
 
+// Represents a single segment of the snake
 class Segment {
     constructor(child) {
         this.child = child;
@@ -15,79 +16,61 @@ class Segment {
         this.angle = 0;
     }
 
-    headX() {
-        return this.pos.x + Math.sin(this.angle) * segmentSize;
+    head() {
+        return new Vec2(this.pos.x + Math.sin(this.angle) * segmentSize,
+                        this.pos.y + Math.cos(this.angle) * segmentSize);
     }
 
-    headY() {
-        return this.pos.y + Math.cos(this.angle) * segmentSize;
+    lookAt(pos) {
+        this.angle = Math.atan2(pos.x - this.pos.x, pos.y - this.pos.y);
     }
 
-    lookAt(x, y) {
-        //this.angle = Math.atan2(x - this.x, y - this.y);
-        this.angle = Math.atan2(x - this.pos.x, y - this.pos.y);
+    moveTowards(pos) {
+        this.lookAt(pos);
+        this.pos.x = pos.x - (this.head().x - this.pos.x);
+        this.pos.y = pos.y - (this.head().y - this.pos.y);
     }
 
-    moveTowards(x, y) {
-        this.lookAt(x, y);
-        this.pos.x = x - (this.headX() - this.pos.x);
-        this.pos.y = y - (this.headY() - this.pos.y);
-    }
-
-    update(x, y) {
-        this.lookAt(x, y);
-        this.moveTowards(x, y);
+    update(pos) {
+        this.moveTowards(pos);
 
         if (this.child) {
-            this.child.update(this.pos.x, this.pos.y);
+            this.child.update(this.pos);
         }
     }
 }
 
 
-function resize(ctx) {
-    let rect = ctx.canvas.getBoundingClientRect();
-    ctx.canvas.width  = rect.width;
-    ctx.canvas.height = rect.height;
-}
-
-
+// Updates and draws the snake
 function draw(ctx, mouseState) {
     if (mouseState.inCanvas) {
-        snake[0].update(mouseState.pos.x, mouseState.pos.y);
+        snake[0].update(mouseState.pos);
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = "white";
 
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = "5";
-        ctx.beginPath();
-        ctx.moveTo(snake[0].headX(), snake[0].headY());
-
-        for (let s of snake) {
-            ctx.lineTo(s.pos.x, s.pos.y);
-        }
-
-        ctx.stroke();
+        snake.forEach(function(s) {
+            ctx.beginPath();
+            ctx.arc(s.pos.x, s.pos.y, segmentSize / 2, 0, Math.PI*2);
+            ctx.fill();
+        });
     }
+
+    handle = requestAnimationFrame(() => draw(ctx, mouseState));
 }
 
 
+// Recreates the snake and starts the simulation
 export function start(ctx, mouseState) {
-    window.addEventListener("resize", function() { resize(ctx); });
-    resize(ctx);
-
-    // Initializing
     snake[segments - 1] = new Segment(null);
     for (let i = segments - 2; i >= 0; --i) {
         snake[i] = new Segment(snake[i + 1]);
     }
 
-    handle = setInterval(function() {
-        draw(ctx, mouseState);
-    }, 33);
+    handle = requestAnimationFrame(() => draw(ctx, mouseState));
 }
 
 
 export function stop() {
-    clearInterval(handle);
+    cancelAnimationFrame(handle);
 }
